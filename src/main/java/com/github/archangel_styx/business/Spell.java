2ccb5c0306c61425d0333e7a14b57d294c303eb6
Cameron Williams <cdwilliams@students.ntc.edu>
2026-08-05 23:39:56 -0500

    changed file directory

---
+++ src/main/java/com/github/archangel_styx/business/Spell.java
@@ -0,0 +1,8 @@
+package com.github.archangel_styx.business;
+
+import com.github.archangel_styx.interfaces.SpellCost;
+
+public abstract class Spell {
+    private String name;
+    private SpellCost spellCost;
+}
---
+++ src/main/java/com/github/archangel_styx/interfaces/SpellCost.java
@@ -0,0 +1,5 @@
+package com.github.archangel_styx.interfaces;
+
+public interface SpellCost {
+
+}
